2ba08a76871e2f6cdf07fd55aee1cb42a9b49025
Rona YE <58823591+YeRona@users.noreply.github.com>
2019-12-12 23:02:11 +0100

    Update 214. Shortest Palindrome_1).py
    use the middle point, trying to solve the last test case, which unfortunately fails

--- 214. Shortest Palindrome_1).py
+++ 214. Shortest Palindrome_1).py
@@ -8,29 +8,33 @@ class Solution:
         if s_len % 2:
             mid = (s_len - 1) // 2
             s_list = list(s)
-            s_rev = s_list[::-1]
+            s_rev = s_list[s_len - 1:mid:-1]
             if s_rev[: mid] == s_list[: mid]:
                 return s
         else:
             mid = (s_len - 2) // 2
             s_list = list(s)
-            s_rev = s_list[::-1]    #make fun of the property of list
+            s_rev = s_list[s_len - 1: mid :-1]    #make fun of the property of list
             if s_rev[: mid + 1] == s_list[:mid + 1]:
                 return s
         for i in range(1, s_len + 1):
             t_len = s_len - i
             t += s[t_len]
+            #print(i, t_len)
             if t_len % 2:
                 mid = (t_len - 1) // 2
                 s_list = list(s)
-                s_rev = s_list[t_len - 1: :-1]
+                s_rev = s_list[t_len - 1:mid:-1]
                 if s_rev[: mid] == s_list[:mid]:
                     t += s
                     return t
             else:
                 mid = (t_len - 2) // 2
+                #print(mid, t_len)
                 s_list = list(s)
-                s_rev = s_list[t_len - 1::-1]
+                s_rev = s_list[t_len - 1:mid:-1]
+                #print(s_list)
+                #print(s_rev)
                 if mid == 0:           #consider the special case
                     if s_list[0] == s_list[1]:
                         t += s
