55750332d505f133e495d62e6d252a99f1cc99d0
Rona YE <58823591+YeRona@users.noreply.github.com>
2020-01-09 02:50:04 +0800

    Update Generate Parentheses.py
    use the dictionary to reduce the repetitive iteration

--- Generate Parentheses.py
+++ Generate Parentheses.py
@@ -12,22 +12,29 @@ For example, given n = 3, a solution set is:
 ]
 '''
 class Solution:
+    dic = {}
     def generateParenthesis(self, n: int) -> List[str]:
         PP = set()
         if n == 1:
             PP = {"()"}
         else:
-            CC = self.generateParenthesis(n - 1)
+            if n - 1 not in self.dic:
+                self.dic[n - 1] = self.generateParenthesis(n - 1)
+            CC = self.dic[n - 1]
             for i in CC:
-                m = "()" + i
-                PP.add(m)
                 m = i + "()"
+                PP.add(m)
+                m = "()" + i
                 PP.add(m)
                 m = "(" + i + ")"
                 PP.add(m)
             for i in range(2, n // 2 + 1):
-                AA = self.generateParenthesis(i)
-                BB = self.generateParenthesis(n - i)
+                if i not in self.dic:
+                    self.dic[i] = self.generateParenthesis(i)
+                if n - i not in self.dic:
+                    self.dic[n - i] = self.generateParenthesis(n - i)
+                AA = self.dic[i]
+                BB = self.dic[n - i]
                 for j in AA:
                     for k in BB:
                         m = j + k
